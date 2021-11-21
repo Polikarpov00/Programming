b4e0646d03c7642a9dd43861d17f5ae04d888085
German <polik20003@gmail.com>
2021-11-21 11:45:20 +0300

    2задания

---
+++ les1/Program.cs
@@ -0,0 +1,23 @@
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("введите число A:");
+
+int numA = int.Parse(Console.ReadLine());
+
+Console.Write("введите число B:");
+
+int numB = int.Parse(Console.ReadLine());
+
+int res = numA / numB; 
+
+Console.WriteLine(res);
+
+if (res == numB)
+{
+    Console.WriteLine("правильно");
+}
+
+else 
+{
+    Console.WriteLine("неправильно");
+}
---
+++ les11/Program.cs
@@ -0,0 +1,35 @@
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("введите число A:");
+
+int numA = int.Parse(Console.ReadLine());
+
+Console.Write("введите число B:");
+
+int numB = int.Parse(Console.ReadLine());
+
+{
+if (numA > numB)
+{
+    Console.Write(numA);
+    Console.WriteLine(" большее число");
+}
+
+else 
+{
+    Console.Write(numA);
+    Console.WriteLine(" меньшее число");
+}
+
+if (numB > numA)
+{
+    Console.Write(numB);
+    Console.WriteLine(" большее число");
+}
+
+else 
+{
+    Console.Write(numB);
+    Console.WriteLine(" меньшее число число");
+    }
+}
