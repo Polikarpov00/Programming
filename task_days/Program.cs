f8cabacefb998ed6265c5cb6bff34b500cf5008e
German <polik20003@gmail.com>
2021-11-21 13:56:40 +0300

    days done

--- task_days/Program.cs
+++ task_days/Program.cs
@@ -1,7 +1,15 @@
 ﻿// По заданному номеру дня недели вывести его название
-string[] array = {0, mon, tue, wed, thu, fri, sat, sun };
 
-Console.Writeline("введите номер дня недели");
 
-int day = int.Parse(Console.Readline());
+string[] week = {"", "mon", "tue", "wed", "thu", "fri", "sat", "sun" };
 
+Console.Write("введите номер дня недели "); 
+
+int day = int.Parse(Console.ReadLine());
+
+Console.Write(String.Join(" ",week.Skip(day).First())); // .Skip(day) - пропускаем элементы массива до введённого нами индекса. 
+//.First() - останавливаемся на первом значении.
+// String.Join - Сцепляет элементы указанного массива или элементы коллекция, помещая между ними заданный разделитель
+
+
+Console.WriteLine();
