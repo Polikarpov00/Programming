312bb55fadb6b7b728422d843fbe10622e28d983
German <polik20003@gmail.com>
2021-11-21 12:05:24 +0300

    3 задания

---
+++ task_days/Program.cs
@@ -0,0 +1,7 @@
+﻿// По заданному номеру дня недели вывести его название
+string[] array = {0, mon, tue, wed, thu, fri, sat, sun };
+
+Console.Writeline("введите номер дня недели");
+
+int day = int.Parse(Console.Readline());
+
