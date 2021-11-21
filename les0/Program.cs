ec464cab2b047381280a9c23b12dc119b497ac01
kirill14994 <94280573+kirill14994@users.noreply.github.com>
2021-11-21 16:40:10 +0300

    add exercise 0

---
+++ les0/Program.cs
@@ -0,0 +1,11 @@
+﻿// найти квадрат числа
+
+Console.Write("Enter number: ");
+
+int number = int.Parse(Console.ReadLine());
+
+//int number = new Random().Next(1, 100);
+//Console.WriteLine(number);
+
+int result = number*number;
+Console.WriteLine(result);
