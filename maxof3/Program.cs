c7efb5534e7ca379b7e1edac0646969c552ae1bf
German <polik20003@gmail.com>
2021-11-22 21:36:48 +0300

    max of 3

---
+++ maxof3/Program.cs
@@ -0,0 +1,32 @@
+﻿//Найти максимальное из трех чисел
+Console.WriteLine("Введите 3 числа:");
+
+int numA = int.Parse(Console.ReadLine());
+int numB = int.Parse(Console.ReadLine());
+int numC = int.Parse(Console.ReadLine());
+
+if (numA > numB)
+ {
+    if (numA>numC)
+    {
+        Console.Write("max is ");
+        Console.WriteLine(numA);
+    }
+    else {
+        Console.Write("max is ");
+        Console.WriteLine(numC);
+    }
+}
+
+else 
+ {
+ if (numB >numC)
+    {
+        Console.Write("max is ");
+        Console.WriteLine(numB);
+    }
+    else {
+        Console.Write("max is ");
+        Console.WriteLine(numC);
+    }
+}
