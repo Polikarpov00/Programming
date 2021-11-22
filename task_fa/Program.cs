c6dc2bf05f0ffd246e20368c0f48014ac9cd7a51
German <polik20003@gmail.com>
2021-11-22 22:11:30 +0300

    max of 3 and f(a) added

---
+++ task_fa/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+
+Console.WriteLine("Input a to find its function");
+
+int a = int.Parse(Console.ReadLine());
+
+int y = 2*a; // Как я понял функцией может быть любое математическое действие. (или как?) У меня  2*a это функция f(a)
+
+Console.WriteLine(y);
+
+
+
+
+
