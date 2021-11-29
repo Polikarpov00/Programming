801c55731b86b3c8e5c0b7ce9bee8170fc68c609
German <polik20003@gmail.com>
2021-11-29 20:20:32 +0300

    17 и 18 задания

---
+++ ex17/Program.cs
@@ -0,0 +1,24 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+
+Console.WriteLine("Введите два числа ");
+
+int a,b;
+
+ a = int.Parse(Console.ReadLine()); 
+ b = int.Parse(Console.ReadLine());
+
+if (a / b == b & a % b ==0)
+{
+    Console.WriteLine($"число {a} является квадратом числа {b} ");
+}
+else if (b / a == a & b % a ==0)
+{
+     Console.WriteLine($"число {b} является квадратом числа {a} ");
+}
+else 
+{
+     Console.WriteLine($"ни одно число не является квадратом другого");
+}
+
+
---
+++ ex18/Program.cs
@@ -0,0 +1,25 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+Console.WriteLine("введите X и Y ");
+int X,Y;
+
+X = int.Parse(Console.ReadLine());
+Y = int.Parse(Console.ReadLine());
+
+if ((X == 0 | X == 1) & (Y == 0 | Y== 1))
+{
+    if ((-(X |Y)) == (-X & -Y)) 
+    {
+        Console.WriteLine("утверждение верно");
+    }
+    else 
+    {
+        Console.WriteLine("неверное утверждение");  
+    }
+
+}
+else 
+{
+    Console.WriteLine("неверные значения");
+}
+
+
