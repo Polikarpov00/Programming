4abecddecae3a508100e3cefbc6990e5073ab841
German <polik20003@gmail.com>
2021-12-12 12:10:00 +0300

    41

--- ex41/Program.cs
+++ ex41/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Выяснить являются ли три числа сторонами треугольника
 Console.WriteLine("Hello, World!");
+double a = 7;  //new Random().Next(0,10);
+double b = 10;  //new Random().Next(0,10);
+double c = 12; //new Random().Next(0,10);
+
